a1e15fbc34be5a4263ae989bdc5f84b5597ebdca
SoulLyoko <519052532@qq.com>
2020-05-13 11:30:16 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 # 确保脚本抛出遇到的错误
-set -e
+# set -e
 
 # 打包生成静态文件
-npm run build
+# npm run build
 
 # 进入打包好的文件夹
 cd docs/.vuepress/dist
@@ -10,6 +10,8 @@ cd docs/.vuepress/dist
 # 创建git的本地仓库，提交修改
 git init
 git add -A
+git config --local user.email "519052532@qq.com"
+git config --local user.name "SoulLyoko"
 git commit -m 'deploy'
 
 # 覆盖式地将本地仓库发布至github，因为发布不需要保留历史记录
