3f1beafdaaf8f77bcdf3c748d1055dfa52dc4efc
Lyoko <519052532@qq.com>
2020-05-13 10:27:05 +0800

    push-github

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# 确保脚本抛出遇到的错误
+set -e
+
+# 打包生成静态文件
+npm run build
+
+# 进入打包好的文件夹
+cd docs/.vuepress/dist
+
+# 创建git的本地仓库，提交修改
+git init
+git add -A
+git commit -m 'deploy'
+
+# 覆盖式地将本地仓库发布至github，因为发布不需要保留历史记录
+# 格式为：git push -f git@github.com:'用户名'/'仓库名'.git master
+git push -f git@github.com:SoulLyoko/soullyoko.github.io.git master
+
+cd -
