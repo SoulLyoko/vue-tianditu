fc6ca3af6196690f94de55070397b32ba988a3e1
SoulLyoko <519052532@qq.com>
2021-03-25 10:40:25 +0800

    update docs

--- docs/.vuepress/components/ex-marker-cluster.vue
+++ docs/.vuepress/components/ex-marker-cluster.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mapDiv">
     <tdt-map :center="center" :zoom="zoom">
-      <tdt-marker-cluster :markers="markers" :styles="styles" @click="clusterClick"></tdt-marker-cluster>
+      <tdt-marker-cluster :markers="markers" @click="clusterClick"></tdt-marker-cluster>
     </tdt-map>
   </div>
 </template>
@@ -23,33 +23,7 @@ export default {
           position: [Math.random() * 40 + 85, Math.random() * 30 + 21],
           extData: "cluster-" + index
         };
-      }),
-      styles: [
-        {
-          url: "http://api.tianditu.gov.cn/img/map/cluster/heart30.png",
-          size: [30, 26], //图片大小
-          offset: [-15, -13], //显示图片的偏移量
-          textColor: "#000000", //显示数字的颜色
-          textSize: 8, //显示文字的大小
-          range: [0, 50]
-        },
-        {
-          url: "http://api.tianditu.gov.cn/img/map/cluster/heart40.png",
-          size: [42, 36],
-          offset: [-20, -17],
-          textColor: "#ff0000",
-          textSize: 10,
-          range: [50, 400]
-        },
-        {
-          url: "http://api.tianditu.gov.cn/img/map/cluster/heart50.png",
-          size: [52, 46],
-          offset: [-10, -22],
-          textColor: "white",
-          textSize: 12,
-          range: [400, 500]
-        }
-      ]
+      })
     };
   },
   methods: {
