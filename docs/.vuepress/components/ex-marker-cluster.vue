47c50871788113c3474ef62c776631d935530eb8
SoulLyoko <519052532@qq.com>
2021-05-18 17:42:25 +0800

    更新点聚合文档的图片

--- docs/.vuepress/components/ex-marker-cluster.vue
+++ docs/.vuepress/components/ex-marker-cluster.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mapDiv">
     <tdt-map :center="center" :zoom="zoom">
-      <tdt-marker-cluster :markers="markers" @click="clusterClick"></tdt-marker-cluster>
+      <tdt-marker-cluster :markers="markers" @click="clusterClick" :styles="styles"></tdt-marker-cluster>
     </tdt-map>
   </div>
 </template>
@@ -18,7 +18,33 @@ export default {
           position: [Math.random() * 40 + 85, Math.random() * 30 + 21],
           extData: "cluster-" + index
         };
-      })
+      }),
+      styles: [
+        {
+          url: "/cluster0.png",
+          size: [30, 30], //图片大小
+          offset: [0, 0], //显示图片的偏移量
+          textColor: "#000000", //显示数字的颜色
+          textSize: 8, //显示文字的大小
+          range: [0, 50] //显示该图标的范围
+        },
+        {
+          url: "/cluster1.png",
+          size: [40, 40],
+          offset: [0, 0],
+          textColor: "#000000",
+          textSize: 10,
+          range: [50, 400]
+        },
+        {
+          url: "/cluster2.png",
+          size: [50, 50],
+          offset: [0, 0],
+          textColor: "#000000",
+          textSize: 12,
+          range: [400, 500]
+        }
+      ]
     };
   },
   methods: {
